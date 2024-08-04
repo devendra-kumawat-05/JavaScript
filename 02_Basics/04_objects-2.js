2ffe60796b163af5ab17a87addb7b9675072092b
Devendra Kumawat <149409257+devendrakumawatt@users.noreply.github.com>
2024-08-04 09:33:10 +0000

    Objects-2 done

---
+++ 02_Basics/04_objects-2.js
@@ -0,0 +1,34 @@
+const tinderUser = new Object();
+// const tinderUser = {}
+
+tinderUser.id = "123abc";
+tinderUser.name = "Dev";
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        username: {
+            Fname: "Devendra",
+            Sname: "Kumawat",
+        }
+    }
+}
+
+// console.log(regularUser.fullname.username.Fname);
+
+const obj1 = {1: "a", 2: "b"};
+const obj2 = {3: "c", 4: "d"};
+
+// const obj3 = {obj1 , obj2};
+// const obj3 = Object.assign({}, obj1,obj2);
+const obj3 = {...obj1,...obj2};
+
+console.log(obj3);
+
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));

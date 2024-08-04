fec387b1ca5d43494fd6d8dde216ccffbad61dd4
Devendra Kumawat <149409257+devendrakumawatt@users.noreply.github.com>
2024-08-04 09:48:17 +0000

    Objects-2 done.

--- 02_Basics/04_objects-2.js
+++ 02_Basics/04_objects-2.js
@@ -26,9 +26,29 @@ const obj2 = {3: "c", 4: "d"};
 // const obj3 = Object.assign({}, obj1,obj2);
 const obj3 = {...obj1,...obj2};
 
-console.log(obj3);
+// console.log(obj3);
 
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+
+const course = {
+    courseName: "JavaScript",
+    price: "999",
+    courseTeacher: "Someone"
+}
+
+const {courseTeacher: Teacher} = course
+// console.log(courseTeacher);
+console.log(Teacher);
+
+/*
+{
+    "name": "Devendra",
+    "courseN": "JavaScript",
+    "price": "Free"
+}
+*/
+

a47c5806a5f5c80b65b5affcc42b325ef41f52a3
Devendra Kumawat <149409257+devendrakumawatt@users.noreply.github.com>
2024-08-11 07:10:53 +0000

    For-in Loop

---
+++ 05_Iterations/for-in-loops.js
@@ -0,0 +1,29 @@
+const myObject = {
+    js : 'Javascript',
+    cpp : 'C++',
+    rb : 'ruby',
+    swift : 'swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shorthand for ${myObject[key]}`);
+}
+
+
+
+const lang = ["js", "rb", "py", "java", "cpp"]
+
+for(const key in lang){
+    // console.log(lang[key]);
+}
+
+
+
+// const map = new Map();
+// map.set('IN', "India");
+// map.set('Fr', "France");
+// map.set('Ca', "Canada");
+
+// for (const key in map) {
+//     console.log(key);
+// }
